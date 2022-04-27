674709b8aeb6fe1111368054f128ba9fc873d7b7
lucassantosdasilva1 <42879994+lucassantosdasilva1@users.noreply.github.com>
2022-04-26 23:10:21 -0300

    solving the problem with image Background

--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -5,28 +5,18 @@ import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #808080;
+  position: absolute;
 
   padding-top: 10px;
   padding-left: 10px;
   padding-right: 10px;
 `;
 
-
-export const ImageWrapper = styled.View`
-  width: 100%;
-  height: 875px;
-  
-  margin-top: 20px;
-  background-color: red;
-`;
-
 export const Image = styled.ImageBackground`
-
+    
 `;
 
 export const Content = styled.View`
-  position: absolute;
 
   margin-top: 400px;
   width: 420px;
