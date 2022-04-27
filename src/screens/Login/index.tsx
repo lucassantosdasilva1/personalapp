2579109e21585ca0a905dee19ab00ad4e2131208
lucassantosdasilva1 <42879994+lucassantosdasilva1@users.noreply.github.com>
2022-04-26 23:09:37 -0300

    Solving the problem with imageBackground

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,48 +1,54 @@
-import React from 'react';
-import { View, StyleSheet, ImageBackground, ImageBackgroundProps } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, ImageBackground, ImageBackgroundProps, Dimensions } from 'react-native';
 
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 import { 
     Container,
     Content,
-    ImageWrapper,
-    Image,
     Title,
     Description,
  } from './styles';
+ 
+ const { width } = Dimensions.get('window');
+
+ const ratio = 644 / 272
 
  const styles = StyleSheet.create({
     stretch: {
         flex: 1,
+        width,
+        height: width * ratio,
         resizeMode: 'cover',
     },
   });
 
-  interface Props extends ImageBackgroundProps{
-
+  interface Props {
+    width: number;
+    height: number;
+    stretched: boolean;
   }
 
-export function Login({resizeMode, style, source, ...rest} : Props) {
+export function Login({width, height, stretched} : Props) {
     const img1 = '../../utils/muier.png';
 
+    const [widthImgBack, setWidthImgBack] = useState(100);
+    const [heightImgBack, setHeightImgBack] = useState(100);
+    
+
     return (
+    <>
+        <ImageBackground 
+            source={require(img1)}
+            resizeMode='cover'
+            style={styles.stretch}
+        />
         <Container>
-            <ImageBackground 
-                 
-                resizeMode='cover'
-                style={styles.stretch}
-                source={require(img1)} 
-            >
-
-
-            <Content>
-                    <Title> Login to your account </Title>
-                    <Description>Good to see you again, enter your details below to continue exercise</Description>
+        <Content>
+                <Title> Login to your account </Title>
+                <Description>Good to see you again, enter your details below to continue exercise</Description>
             </Content>
-
-            </ImageBackground>
         </Container>
-
+    </>
     )
 }
